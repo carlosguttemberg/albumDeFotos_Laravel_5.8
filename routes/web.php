a7e5c903ecebdc60159b1d55684b2b26673ee9d0
carlosguttemberg <guttembergcarlos@gmail.com>
2020-02-24 13:01:20 -0300

    Adicionada a funcao de download de arquivos

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@
 Route::get('/', 'PostControlador@index');
 Route::post('/', 'PostControlador@store');
 Route::delete('/{id}', 'PostControlador@destroy');
+Route::get('/download/{id}', 'PostControlador@download');
